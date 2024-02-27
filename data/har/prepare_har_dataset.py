0b8c65d7878c8920b12eb377c30e01f9a1de5514
mbisan <mbikandi@bcamath.org>
2024-02-27 15:52:34 +0100

    add mhealth and pamap preparation code

--- data/har/prepare_har_dataset.py
+++ data/har/prepare_har_dataset.py
@@ -23,7 +23,9 @@ DATASETS = {
     "REALDISP": "https://archive.ics.uci.edu/static/public/305/realdisp+activity+recognition+dataset.zip",
     "DAPHNET-FOG": "https://archive.ics.uci.edu/static/public/245/daphnet+freezing+of+gait.zip",
     "MHEALTH": "https://archive.ics.uci.edu/static/public/319/mhealth+dataset.zip",
-    "TempestaTMD": "https://tempesta.cs.unibo.it/projects/us-tm2017/static/dataset/raw_data/raw_data.tar.gz" # tar
+    "TempestaTMD": "https://tempesta.cs.unibo.it/projects/us-tm2017/static/dataset/raw_data/raw_data.tar.gz", # tar
+    "MHEALTH": "https://archive.ics.uci.edu/static/public/319/mhealth+dataset.zip",
+    "PAMAP2": "https://archive.ics.uci.edu/static/public/231/pamap2+physical+activity+monitoring.zip"
 }
 
 def download(dataset_name = "all", dataset_dir=None):
@@ -269,6 +271,130 @@ def prepare_wisdm(dataset_dir):
         with open(os.path.join(dataset_dir, f"subject_{user_id}_class.npy"), "wb") as f:
             np.save(f, np.array(labels, dtype=int))
 
+def prepare_mhealth(dataset_dir):
+    subject_files = list(filter(lambda x: "subject" in x, os.listdir(os.path.join(dataset_dir, "MHEALTHDATASET"))))
+    subject_files.sort()
+
+    mhealth_names = [
+        "C-acc_x",  # Column 1: acceleration from the chest sensor (X axis)
+        "C-acc_y",  # Column 2: acceleration from the chest sensor (Y axis)
+        "C-acc_z",  # Column 3: acceleration from the chest sensor (Z axis)
+        "ECG_lead1",  # Column 4: electrocardiogram signal (lead 1)
+        "ECG_lead2",  # Column 5: electrocardiogram signal (lead 2)
+        "LA-acc_x",  # Column 6: acceleration from the left-ankle sensor (X axis)
+        "LA-acc_y",  # Column 7: acceleration from the left-ankle sensor (Y axis)
+        "LA-acc_z",  # Column 8: acceleration from the left-ankle sensor (Z axis)
+        "LA-gyro_x",  # Column 9: gyro from the left-ankle sensor (X axis)
+        "LA-gyro_y",  # Column 10: gyro from the left-ankle sensor (Y axis)
+        "LA-gyro_z",  # Column 11: gyro from the left-ankle sensor (Z axis)
+        "LA-mag_x",  # Column 12: magnetometer from the left-ankle sensor (X axis)
+        "LA-mag_y",  # Column 13: magnetometer from the left-ankle sensor (Y axis)
+        "LA-mag_z",  # Column 14: magnetometer from the left-ankle sensor (Z axis)
+        "RA-acc_x",  # Column 15: acceleration from the right-lower-arm sensor (X axis)
+        "RA-acc_y",  # Column 16: acceleration from the right-lower-arm sensor (Y axis)
+        "RA-acc_z",  # Column 17: acceleration from the right-lower-arm sensor (Z axis)
+        "RA-gyro_x",  # Column 18: gyro from the right-lower-arm sensor (X axis)
+        "RA-gyro_y",  # Column 19: gyro from the right-lower-arm sensor (Y axis)
+        "RA-gyro_z",  # Column 20: gyro from the right-lower-arm sensor (Z axis)
+        "RA-mag_x",  # Column 21: magnetometer from the right-lower-arm sensor (X axis)
+        "RA-mag_y",  # Column 22: magnetometer from the right-lower-arm sensor (Y axis)
+        "RA-mag_z",  # Column 23: magnetometer from the right-lower-arm sensor (Z axis)
+        "Label"  # Column 24: Label (0 for the null class)
+    ]
+
+    sensor_groups = {
+        "chest_acc": ["C-acc_x", "C-acc_y", "C-acc_z"],
+        "ECG": ["ECG_lead1", "ECG_lead2"],
+        "left_ankle": ["LA-acc_x", "LA-acc_y", "LA-acc_z", "LA-gyro_x", "LA-gyro_y", "LA-gyro_z", "LA-mag_x", "LA-mag_y", "LA-mag_z"],
+        "right_lower_arm": ["RA-acc_x", "RA-acc_y", "RA-acc_z", "RA-gyro_x", "RA-gyro_y", "RA-gyro_z", "RA-mag_x", "RA-mag_y", "RA-mag_z"],
+        "label": ["Label"]
+    }
+
+    for subject in subject_files:
+        df = pandas.read_csv(os.path.join(dataset_dir, "MHEALTHDATASET", subject), 
+            sep="\t", header=None, 
+            names=mhealth_names)
+        
+        subject_folder = os.path.join(dataset_dir, subject.replace(".log", ""))
+
+        if not os.path.exists(subject_folder):
+            os.mkdir(subject_folder)
+
+        for name, value in sensor_groups.items():
+
+            with open(os.path.join(subject_folder, f"{name}.npy"), "wb") as f:
+                np.save(f, df[value].to_numpy())
+
+def prepare_pamap2(dataset_dir):
+    subject_files_0 = list(filter(lambda x: "subject" in x, os.listdir(os.path.join(dataset_dir, "PAMAP2_Dataset", "Protocol"))))
+    subject_files_1 = list(filter(lambda x: "subject" in x, os.listdir(os.path.join(dataset_dir, "PAMAP2_Dataset", "Optional"))))
+
+    column_names = [
+        "timestamp",  # Column 1: timestamp (s)
+        "activityID",  # Column 2: activityID
+        "heart_rate",  # Column 3: heart rate (bpm)
+        # Hand IMU
+        "hand_temp",  # Column 4: temperature (°C) for hand IMU
+        "hand_acc_x", "hand_acc_y", "hand_acc_z",  # Column 5-7: 3D-acceleration data for hand IMU (ms-2)
+        "hand_acc_x_6g", "hand_acc_y_6g", "hand_acc_z_6g",  # Column 8-10: 3D-acceleration data (±6g) for hand IMU (ms-2)
+        "hand_gyro_x", "hand_gyro_y", "hand_gyro_z",  # Column 11-13: 3D-gyroscope data for hand IMU (rad/s)
+        "hand_mag_x", "hand_mag_y", "hand_mag_z",  # Column 14-16: 3D-magnetometer data for hand IMU (μT)
+        "hand_orientation_1", "hand_orientation_2", "hand_orientation_3", "hand_orientation_4",  # Column 17-20: orientation data for hand IMU
+        # Chest IMU
+        "chest_temp",  # Column 21: temperature (°C) for chest IMU
+        "chest_acc_x", "chest_acc_y", "chest_acc_z",  # Column 22-24: 3D-acceleration data for chest IMU (ms-2)
+        "chest_acc_x_6g", "chest_acc_y_6g", "chest_acc_z_6g",  # Column 25-27: 3D-acceleration data (±6g) for chest IMU (ms-2)
+        "chest_gyro_x", "chest_gyro_y", "chest_gyro_z",  # Column 28-30: 3D-gyroscope data for chest IMU (rad/s)
+        "chest_mag_x", "chest_mag_y", "chest_mag_z",  # Column 31-33: 3D-magnetometer data for chest IMU (μT)
+        "chest_orientation_1", "chest_orientation_2", "chest_orientation_3", "chest_orientation_4",  # Column 34-37: orientation data for chest IMU
+        # Ankle IMU
+        "ankle_temp",  # Column 38: temperature (°C) for ankle IMU
+        "ankle_acc_x", "ankle_acc_y", "ankle_acc_z",  # Column 39-41: 3D-acceleration data for ankle IMU (ms-2)
+        "ankle_acc_x_6g", "ankle_acc_y_6g", "ankle_acc_z_6g",  # Column 42-44: 3D-acceleration data (±6g) for ankle IMU (ms-2)
+        "ankle_gyro_x", "ankle_gyro_y", "ankle_gyro_z",  # Column 45-47: 3D-gyroscope data for ankle IMU (rad/s)
+        "ankle_mag_x", "ankle_mag_y", "ankle_mag_z",  # Column 48-50: 3D-magnetometer data for ankle IMU (μT)
+        "ankle_orientation_1", "ankle_orientation_2", "ankle_orientation_3", "ankle_orientation_4",  # Column 51-54: orientation data for ankle IMU
+    ]
+
+    sensor_groups = {
+        "time": "timestamp",  # Column 1: timestamp (s)
+        "label": "activityID",  # Column 2: activityID
+        "HR": "heart_rate",
+        "hand_temp": "hand_temp",
+        "hand_acc": ["hand_acc_x", "hand_acc_y", "hand_acc_z"],
+        "hand_acc_6g": ["hand_acc_x_6g", "hand_acc_y_6g", "hand_acc_z_6g"],
+        "hand_gyro": ["hand_gyro_x", "hand_gyro_y", "hand_gyro_z"],
+        "hand_mag": ["hand_mag_x", "hand_mag_y", "hand_mag_z"],
+        "hand_orientation": ["hand_orientation_1", "hand_orientation_2", "hand_orientation_3", "hand_orientation_4"],
+        "chest_temp": "chest_temp",
+        "chest_acc": ["chest_acc_x", "chest_acc_y", "chest_acc_z"],
+        "chest_acc_6g": ["chest_acc_x_6g", "chest_acc_y_6g", "chest_acc_z_6g"],
+        "chest_gyro": ["chest_gyro_x", "chest_gyro_y", "chest_gyro_z"],
+        "chest_mag": ["chest_mag_x", "chest_mag_y", "chest_mag_z"],
+        "chest_orientation": ["chest_orientation_1", "chest_orientation_2", "chest_orientation_3", "chest_orientation_4"],
+        "ankle_temp": "ankle_temp",
+        "ankle_acc": ["ankle_acc_x", "ankle_acc_y", "ankle_acc_z"],
+        "ankle_acc_6g": ["ankle_acc_x_6g", "ankle_acc_y_6g", "ankle_acc_z_6g"],
+        "ankle_gyro": ["ankle_gyro_x", "ankle_gyro_y", "ankle_gyro_z"],
+        "ankle_mag": ["ankle_mag_x", "ankle_mag_y", "ankle_mag_z"],
+        "ankle_orientation": ["ankle_orientation_1", "ankle_orientation_2", "ankle_orientation_3", "ankle_orientation_4"]
+    }
+
+    for i, subjects in enumerate([subject_files_0, subject_files_1]):
+        for subject in subjects:
+            df = pandas.read_csv(os.path.join(dataset_dir, "PAMAP2_Dataset", "Protocol" if i==0 else "Optional", subject), 
+                sep=" ", header=None, 
+                names=column_names)
+            
+            subject_folder = os.path.join(dataset_dir, subject.replace(".dat", ""))
+
+            if not os.path.exists(subject_folder):
+                os.mkdir(subject_folder)
+
+            for name, value in sensor_groups.items():
+
+                with open(os.path.join(subject_folder, f"{name}{i}.npy"), "wb") as f:
+                    np.save(f, df[value].to_numpy())
 
 if __name__ == "__main__":
     if not os.path.exists("./datasets"):
@@ -277,11 +403,17 @@ if __name__ == "__main__":
     download("UCI-HAR", "./datasets")
     download("HARTH", "./datasets")
     download("WISDM", "./datasets")
+    download("MHEALTH", "./datasets")
+    download("PAMAP2", "./datasets")
 
+    unpack("PAMAP2", "./datasets")
+    unpack("MHEALTH", "./datasets")
     unpack("UCI-HAR", "./datasets")
     unpack("HARTH", "./datasets")
     unpack("WISDM", "./datasets")
-
+    
+    prepare_mhealth("./datasets/MHEALTH")
+    prepare_pamap2("./datasets/PAMAP2")
     prepare_uci_har("./datasets/UCI-HAR")
     prepare_harth("./datasets/HARTH")
     prepare_wisdm("./datasets/WISDM")
