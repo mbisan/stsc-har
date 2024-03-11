7d36af46aec1ed0aa933ca6eaddb4cf5927a499c
mbisan <mbikandi@bcamath.org>
2024-03-11 12:42:14 +0100

    change reduce_imbalance method

--- data/methods.py
+++ data/methods.py
@@ -13,7 +13,7 @@ def reduce_imbalance(train_indices, stsds: STSDataset, _, include_change_points 
     for i in range(cl.shape[0]):
         train_label_weights[train_labels == cl[i]] = 1 / counts[i]
 
-    examples_per_epoch = int(np.mean(counts))
+    examples_per_epoch = int(np.min(counts))*cl.shape[0]
 
     # add change points to the training indices
     # (a change point from 1/3 up to 2/3 of the leading points in the time series)
@@ -33,6 +33,7 @@ def reduce_imbalance(train_indices, stsds: STSDataset, _, include_change_points 
                  np.full_like(train_change_points, 1/train_change_points.shape[0])])
 
     print(f"Sampling {examples_per_epoch} (balanced) observations per epoch.")
-    train_sampler = WeightedRandomSampler(train_label_weights, examples_per_epoch, replacement=True)
+    train_sampler = WeightedRandomSampler(
+        train_label_weights, examples_per_epoch, replacement=False)
 
     return train_indices, train_sampler
